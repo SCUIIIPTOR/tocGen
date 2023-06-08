f12c714e09807f6a151a4e60ed0c63446cf716ef
Олег Ларин <oleg.larin.2016@list.ru>
2023-06-08 20:38:08 +0300

    Добавлено перечисление TagType

--- tocGen/tocGen/tocgen.h
+++ tocGen/tocGen/tocgen.h
@@ -1,1 +1,25 @@
+/*!
+*\file
+*\brief Данный  файл содержит в себе заголовки функций, которые используются в ходе работы программы tocGen.
+*/
+
 #include <QCoreApplication>
+#include <QFile>
+#include <QRegularExpression>
+#include <stdio.h>
+using namespace std;
+
+static QRegularExpression headerTags("(?<!<)<h([1-6])[^>]*>(?!>)|(?<!<)</h([1-6])[^>]*>(?!>)");
+static QRegularExpression comments("<!--\\s*(.*?)\\s*-->");
+
+/*!
+* \brief Перечисление типов тега в HTML-коде
+*/
+enum TagType
+{
+    OPEN_TAG = 0,
+    CLOSE_TAG = 1,
+    UNKNOWN_TAG = 2
+};
+
+
